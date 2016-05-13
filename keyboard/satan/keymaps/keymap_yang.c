6dd5873c276f94cf4d7c0b1c595fd405b412dc9b
Bektur <ryskeldiev.b@gmail.com>
2016-05-13 13:22:25 +0900

    Updated to support 62 keys and HHKB-style fn layer

--- keyboard/satan/keymaps/keymap_yang.c
+++ keyboard/satan/keymaps/keymap_yang.c
@@ -29,28 +29,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BW] = KEYMAP(
   F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,  KC_EQL,   KC_BSPC, \
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
-  F(11),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,            KC_ENT,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,            KC_RSFT, \
-  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                       KC_RALT,KC_RGUI,  KC_RCTL,  F(9)),
-  /* Keymap _BM: (Base Mac) Mac Default Layer
-   * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|
-   * |-----------------------------------------------------------|
-   * |CAPS/FL|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |
-   * |-----------------------------------------------------------|
-   * |Ctrl|Alt |Gui |      Space            |Gui |Alt |CTRL |_FL |
-   * `-----------------------------------------------------------'
-   */
-[_BM] = KEYMAP(
-  F(0),    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,  KC_EQL,   KC_BSPC, \
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,  KC_RBRC,  KC_BSLS, \
-  F(11),   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,            KC_ENT,  \
-  KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,            KC_RSFT, \
-  KC_LCTL, KC_LALT,KC_LGUI,          KC_SPC,                                       KC_RGUI,KC_RALT,  KC_RCTL,  F(9)),
+  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,            KC_ENT,\
+  KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          F(11),    KC_RSFT, \
+  KC_LCTL, KC_LGUI,KC_LALT,          KC_SPC,                                       KC_LEFT,KC_DOWN,  KC_UP,    KC_RIGHT),
   /* Keymap _FL: Function Layer
    * ,-----------------------------------------------------------.
    * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
@@ -66,31 +47,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [_FL] = KEYMAP(
   KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_DEL,  \
-  KC_TRNS,KC_TRNS,KC_UP,  KC_TRNS,KC_CALC,KC_INS, KC_TRNS,KC_PGUP,KC_TRNS, KC_TRNS,   KC_PSCR,   KC_SLCK,   KC_PAUS,   KC_TRNS, \
-  KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_HOME,KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT,   KC_END,    KC_TRNS,              KC_TRNS, \
-  KC_TRNS,        BL_DEC, BL_TOGG,BL_INC, M(0),   KC_TRNS,KC_PGDN,KC_TRNS, KC_VOLD,   KC_MUTE,   KC_VOLU,              KC_PGUP, \
-  KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,                               KC_APP,    KC_HOME,   KC_PGDN,              KC_END),
-  /* Keymap 3: Fn1 Layer
-   * ,-----------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |
-   * |-----------------------------------------------------------|
-   * |     |   |Up |   |   |   |   |   |   |   |   |BL-|BL+|BL   |
-   * |-----------------------------------------------------------|
-   * |      |Lef|Dow|Rig|   |Hom|Lef|Dow|Up |Rig|End|   |        |
-   * |-----------------------------------------------------------|
-   * |        | F1|F2 | F3|F4 | F5| F6| F7| F8|   |   |Up        |
-   * |-----------------------------------------------------------|
-   * |    |    |    |         FN9            |    |Left|Down|Rig |
-   * `-----------------------------------------------------------'
-   */
-[_MV] = KEYMAP(
-  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
-  KC_NO,  KC_NO,  KC_UP,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  BL_DEC, BL_INC, BL_TOGG, \
-  KC_NO,  KC_LEFT,KC_DOWN,KC_RGHT,KC_NO,  KC_HOME,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_END, KC_NO,          KC_NO, \
-  KC_NO,          F(1),   F(2),   F(3),   F(4),   F(5),   F(6),   F(7),   F(8),   KC_NO,  KC_NO,          KC_UP, \
-  KC_NO,  KC_NO,  KC_NO,         KC_TRNS,                                         F(10),  KC_LEFT,KC_DOWN,KC_RGHT),
-
-
+  KC_TRNS,KC_TRNS,KC_UP,  KC_TRNS,KC_CALC,KC_INS, KC_TRNS,KC_PGUP,KC_TRNS, KC_TRNS,   KC_PSCR,   KC_UP,     KC_PAUS,   KC_TRNS, \
+  KC_TRNS,KC_LEFT,KC_DOWN,KC_RGHT,KC_TRNS,KC_HOME,KC_LEFT,KC_DOWN,KC_UP,   KC_RGHT,   KC_RIGHT,  KC_LEFT,              KC_TRNS, \
+  KC_TRNS,        BL_DEC, BL_TOGG,BL_INC, M(0),   KC_TRNS,KC_PGDN,KC_TRNS, KC_VOLD,   KC_MUTE,   KC_DOWN,   KC_TRNS,   KC_RSFT, \
+  KC_TRNS,KC_TRNS,KC_TRNS,          KC_TRNS,                               KC_APP,    KC_HOME,   KC_PGDN,              KC_END)
 };
 
 
@@ -118,7 +78,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [8]  = ACTION_FUNCTION(RGBLED_DECREASE_VAL),
   [9]  = ACTION_LAYER_ON(_MV, ON_PRESS),
   [10] = ACTION_LAYER_OFF(_MV, ON_PRESS),
-  [11] = ACTION_LAYER_TAP_KEY(_FL, KC_CAPS),
+  [11] = ACTION_LAYER_MOMENTARY(_FL),
 };
 
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
